3cac02ac8af8140580a667770bd50b25a630d67c
Trevor Perrin <github@trevp.net>
2017-06-19 17:12:26 +0000

    calculateVrfSignature, verifyVrfSignature updated for new generalized VRF.

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -10,8 +10,8 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE     := libcurve25519-ref10
-LOCAL_SRC_FILES  := $(wildcard ed25519/*.c) $(wildcard ed25519/additions/*.c) $(wildcard ed25519/nacl_sha512/*.c)
-LOCAL_C_INCLUDES := ed25519/nacl_includes ed25519/additions ed25519/sha512 ed25519
+LOCAL_SRC_FILES  := $(wildcard ed25519/*.c) $(wildcard ed25519/additions/*.c) $(wildcard ed25519/additions/generalized/*.c)  $(wildcard ed25519/nacl_sha512/*.c)
+LOCAL_C_INCLUDES := ed25519/nacl_includes ed25519/additions ed25519/additions/generalized ed25519/sha512 ed25519
 
 include $(BUILD_STATIC_LIBRARY)
 
@@ -19,7 +19,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE     := libcurve25519
 LOCAL_SRC_FILES  := curve25519-jni.c
-LOCAL_C_INCLUDES := ed25519/additions
+LOCAL_C_INCLUDES := ed25519/additions ed25519/additions/generalized
 
 LOCAL_STATIC_LIBRARIES := libcurve25519-donna libcurve25519-ref10
 
