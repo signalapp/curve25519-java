b54a289c76657a2a0a51f1ff097734fb676537ba
Trevor Perrin <github@trevp.net>
2017-06-19 17:43:21 +0000

    Add Makefile and main.c for testing

--- android/jni/ed25519/main/main.c
+++ android/jni/ed25519/main/main.c
@@ -1,9 +1,13 @@
 #include "tests.h"
+#include "gen_tests.h"
 
 
 int main(int argc, char* argv[])
 {
+  generalized_all_fast_tests(0);
   all_fast_tests(0);
+
+  generalized_xveddsa_slow_test(0, 10000);
   curvesigs_slow_test(0,           10000);
   xeddsa_slow_test(0,              10000);
   xeddsa_to_curvesigs_slow_test(0, 10000);
