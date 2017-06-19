b71b9f135d3941ca7e8eafd3807f68e688dd5891
Trevor Perrin <github@trevp.net>
2017-06-19 17:06:57 +0000

    Generalized EdDSA, new VRF
    Closes #21
    // FREEBIE

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -10,8 +10,11 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE     := libcurve25519-ref10
-LOCAL_SRC_FILES  := $(wildcard ed25519/*.c) $(wildcard ed25519/additions/*.c) $(wildcard ed25519/nacl_sha512/*.c)
-LOCAL_C_INCLUDES := ed25519/nacl_includes ed25519/additions ed25519/sha512 ed25519
+LOCAL_SRC_FILES  := $(wildcard ed25519/*.c) $(wildcard ed25519/additions/*.c) \
+                    $(wildcard ed25519/additions/generalized/*.c) $(wildcard ed25519/nacl_sha512/*.c) \
+		    ed25519/tests/internal_fast_tests.c
+
+LOCAL_C_INCLUDES := ed25519/nacl_includes ed25519/additions ed25519/additions/generalized ed25519/tests ed25519/sha512 ed25519
 
 include $(BUILD_STATIC_LIBRARY)
 
@@ -19,7 +22,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE     := libcurve25519
 LOCAL_SRC_FILES  := curve25519-jni.c
-LOCAL_C_INCLUDES := ed25519/additions
+LOCAL_C_INCLUDES := ed25519/additions ed25519/additions/generalized ed25519/tests
 
 LOCAL_STATIC_LIBRARIES := libcurve25519-donna libcurve25519-ref10
 
--- android/src/androidTest/java/org/whispersystems/curve25519/NativeCurve25519ProviderTest.java
+++ android/src/androidTest/java/org/whispersystems/curve25519/NativeCurve25519ProviderTest.java
@@ -5,4 +5,12 @@ public class NativeCurve25519ProviderTest extends Curve25519ProviderTest {
   protected Curve25519Provider createProvider() throws NoSuchProviderException {
     return new NativeCurve25519Provider();
   }
+
+  private native boolean internalTest();
+
+  public void testInternalTest() throws Exception {
+    assertTrue(internalTest());
+  }
+
+
 }
